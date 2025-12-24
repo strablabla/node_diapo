969db8354142d594b805d31c28e0a74658a09f1a
lio3867 <{lionel.chiron@gmail.com}>
2025-12-24 22:54:08 +0100

    websocket in lib

--- html_app.js
+++ html_app.js
@@ -1,7 +1,5 @@
 var express = require('express')
-var path = require("path");
 var open = require('open');
-var fs = require('fs');
 var _ = require('underscore');
 var countFiles = require('count-files');
 var nunjucks  = require('nunjucks');
@@ -16,11 +14,11 @@ var update_viewport_dimensions = require('./lib/update_viewport');
 var cleanupOldPDFs = require('./lib/cleanup_pdfs');
 var concat_diapos = require('./lib/concat_diapos');
 var thumbnails = require('./lib/thumbnails');
-var handle_upload_image = require('./lib/upload_image');
+var uploadImage = require('./lib/upload_image');
 var slideOperations = require('./lib/slide_operations');
+var routing = require('./lib/routing');
+var setupWebSocket = require('./lib/websocket');
 var domtoimage = require('dom-to-image');
-var multer = require('multer');
-var crypto = require('crypto');
 
 String.prototype.format = function () {
     var i = 0, args = arguments;
@@ -34,11 +32,7 @@ String.prototype.format = function () {
 var app = express()
 var server = require('http').createServer(app);
 
-var patt = '' // pattern for scroll position
-var scroll_html_pos = 0 //
 var comment = false;
-diapo_index = 0;
-var fullscreen = false
 
 var port = 3067
 server.listen(port);
@@ -52,96 +46,15 @@ nunjucks.configure('views', {
     watch:true
 });
 
-function addget(app,i){           // add Routes
-
-    var addrd = '/d{}'.format(i)
-    var adddiap = 'diapos/diapo.html'
-    console.log(addrd + '__' + adddiap)
-
-    app.get(addrd, function(req, res){
-        fs.readFile("views/config/config.json", 'utf8', function (err, config_contents) {
-                if (err) { return console.log(err); }
-                var config_params = JSON.parse(config_contents);
-                // Add diapo_index to the parameters
-                config_params.diapo_index = i;
-                res.render(adddiap, config_params);
-        });
-    });
-    concat_diapos(i, numdiap)              // concatenate all the diapo in one file
-}
-
 // ----------------  Make the Routage
 
 numdiap = null      // number of slides
 
-function main_init(){
-
-      /*
-      Routage and concatenation
-      */
-
-      
-
-      // Count only d*.html files (markdown files), not diapo*.html (jinja templates)
-      fs.readdir('views/diapos', function(err, files) {
-            if (err) {
-                  console.error('Error reading diapos directory:', err)
-                  return
-            }
-
-            // Filter to only count d[0-9]*.html files (not diapo*.html)
-            var dFiles = files.filter(function(file) {
-                  return file.match(/^d\d+\.html$/)
-            })
-
-            numdiap = dFiles.length
-            console.log('done counting')
-            console.log('Found', dFiles.length, 'd*.html files')
-            console.log('numdiap is ' + numdiap)
-
-            for (var i=0; i < numdiap; i++){
-                  addget(app,i)      // Routage
-            } // end for
-      }) // end readdir
-
-}
-
-function other_routes(){
-
-      /*
-      Routing text, all and all_mini..
-      */
-
-      fs.readFile("views/config/config.json", 'utf8', function (err, config_contents) {
-            if (err) { return console.log(err); }
-            //-----------
-            json_params = JSON.parse(config_contents);
-            var dict_numdiap = { "number_diapos" : numdiap }
-            var config_params = Object.assign({}, json_params, dict_numdiap);
-            //-----------
-            app.get('/text', function(req, res){ res.render('text.html', config_params) });
-            app.get('/all', function(req, res){ res.render('diapo_all.html', config_params) });
-            app.get('/all_mini', function(req, res){ res.render('diapo_all_small.html', config_params) });
-        });
-
-}
-
-//-------------------- Make all the routings..
-
-function route_all(){
-
-  /*
-  Routing all
-  */
-
-  main_init()
-  other_routes()
-
-}
-
 //------------------- Routage
 
-route_all()
+routing.route_all(app, concat_diapos, function(num) {
+    numdiap = num
+})
 
 //--------------  static addresses
 
@@ -151,180 +64,35 @@ app.use(express.static('lib'));
 app.use('/thumbnails', express.static('views/thumbnails'));
 app.use('/static', express.static('static'));
 
-//--------------  Image upload configuration
-
-// Configure multer for image uploads
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/imgs/')
-    },
-    filename: function (req, file, cb) {
-        // Generate unique filename
-        var ext = path.extname(file.originalname)
-        var basename = path.basename(file.originalname, ext)
-        var uniqueName = basename + '-' + Date.now() + ext
-        cb(null, uniqueName)
-    }
-})
-
-var upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-        // Accept images only
-        if (!file.mimetype.startsWith('image/')) {
-            return cb(new Error('Only image files are allowed!'), false)
-        }
-        cb(null, true)
-    }
-})
-
-// Image upload endpoint
-app.post('/upload-image', upload.single('image'), function(req, res) {
-    handle_upload_image(req, res, diapo_index, modify)
-})
-
-
 //--------------  websocket
-
-// Loading socket.io
 
 var io = require('socket.io')(server);
 
-io.sockets.on('connection', function (socket) {
+// Setup websocket handlers and get shared state
+var websocketState = setupWebSocket(io, server, {
+    port: port,
+    app: app,
+    numdiap: numdiap,
+    count: count,
+    util: util,
+    re: re,
+    modify: modify,
+    findpos_modif_txt: findpos_modif_txt,
+    imagePosition: imagePosition,
+    generate_pdf_from_thumbnails: generate_pdf_from_thumbnails,
+    update_viewport_dimensions: update_viewport_dimensions,
+    thumbnails: thumbnails,
+    slideOperations: slideOperations,
+    routing: routing,
+    concat_diapos: concat_diapos
+})
 
-      console.log('A client is connected!');
+//--------------  Image upload configuration
 
-      socket.emit('numdiap', diapo_index)
-      socket.emit('maxdiap', numdiap) // diapo_max
-
-      socket.on('numdiap', function(text){
-            diapo_index = text
-            console.log('in html_apps, diapo_index is ' + diapo_index)
-
-          })
-
-      fs.readFile('views/diapos/d{}.html'.format(diapo_index), 'utf8', function (err,text) {
-              if (err) { return console.log(err); }
-              re.emit_from_read(socket, count, patt, text, scroll_html_pos)
-          });                        // end fs.readFile
-      util.save_regularly()          // save regularly the text..
-      socket.on('join', function(data) { socket.emit('scroll', patt) }); // end socket.on join
-
-      //-----------------  Read the file all_diap.md
-
-      fs.readFile('views/saved/all_diap.md', 'utf8', function (err,txt) {
-            if (err) { return console.log(err); }
-            //console.log(txt)
-            io.emit('tags_voice',txt)
-
-            }) // end readFile
-
-      //-------------------------------- From textarea to html
-
-      socket.on('return', function(new_text) {       // change html with textarea
-            modify.modify_html_with_newtext(socket, fs, util, new_text, diapo_index)
-            // Update thumbnail after saving
-            thumbnails.updateSingleThumbnail(diapo_index, port).catch(err => {
-                console.error('Error updating thumbnail:', err)
-            })
-         }); // end socket.on return
-
-      //---------------------------------------- Make a new diapo..
-
-      socket.on('make_new_diap', function(){           // Ctrl + D
-            modify.modify_html_with_newtext(socket, fs, util, ' ', numdiap)
-            main_init()
-          })
-
-      //---------------------------------------- Show the memos
-
-      socket.on('show_memos', function(){             // Ctrl + M
-            console.log('#####################  received show memos !!!! ')
-            io.emit('trigger_memos', ''); // triggers memos in diapo.html..
-          })
-
-      //---------------------------------------- Full screen
-
-      socket.on('full_screen', function(){             //
-            console.log('#####################  full screen !!!! ')
-            fullscreen = !fullscreen
-            io.emit('full_screen', ''); //
-          })
-
-      //---------------------------------------- go to md
-
-      socket.on('markdown', function(){             //
-            console.log('###### go to md !!!! ')
-            io.emit('markdown', ''); //
-          })
-
-      //---------------------------------------- show syntax
-
-      socket.on('syntax', function(){             //
-            console.log('###### syntax!!!! ')
-            io.emit('syntax', ''); //
-          })
-
-      //---------------------------------------- show config
-
-      socket.on('config', function(){             //
-            console.log('###### config!!!! ')
-            io.emit('config', ''); //
-          })
-
-      //---------------------------------  Scroll
-
-      socket.on('scroll', function(pattern) { patt = pattern })
-      socket.on('scroll_html', function(pos) { scroll_html_pos = pos })
-
-      //---------------------------------  Delete
-
-      socket.on('delete', function(namediap) {
-
-          slideOperations.delete_slide(namediap)
-
-          //------------------- renumber
-
-          for (i = parseInt(namediap) + 1; i < numdiap ; i++ ){
-
-              slideOperations.shift_renumber_slides(i, numdiap, modify)
-
-          }
-
-          //------------------ reroute all after delete
-
-          route_all()
-
-        }); // end delete
-
-      //---------------------------------   Image position..
-
-      socket.on('pos_img', function(infos){
-          imagePosition.save_image_position(infos, diapo_index, findpos_modif_txt)
-      })
-
-      //---------------------------------   Update thumbnail after Ctrl+S
-
-      socket.on('update_thumbnail', function(){
-          thumbnails.updateSingleThumbnail(diapo_index, port).catch(err => {
-              console.error('Error updating thumbnail:', err)
-          })
-      })
-
-      //---------------------------------   Update viewport dimensions from client
-
-      socket.on('update_viewport', function(dimensions){
-          update_viewport_dimensions(dimensions)
-      })
-
-      //---------------------------------   Generate PDF from thumbnails
-
-      socket.on('generate_pdf', function(){
-          generate_pdf_from_thumbnails(socket, numdiap)
-      })
-
-
-}); // end sockets.on connection
+// Image upload endpoint (uses websocketState.diapo_index)
+app.post('/upload-image', uploadImage.upload.single('image'), function(req, res) {
+    uploadImage.handle_upload_image(req, res, websocketState.diapo_index, modify)
+})
 
 
 console.log('Server running at {}'.format(addr));
